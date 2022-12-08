8fa525fc578b52c991d227f9af62bafb6d4d8c93
Seohui Park <parkseohuinim@gmail.com>
2022-12-08 13:31:54 +0900

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { readFileSync } from 'fs';
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
